0d6e0cc638a1cd11fbb6c6ef477ae8b61db8ad91
ChristopherAumonte <christopheraumonte@gmail.com>
2024-01-08 07:10:10 -0500

    Version 32

--- appacademy/project/frontend/src/components/Navigation/Navigation.jsx
+++ appacademy/project/frontend/src/components/Navigation/Navigation.jsx
@@ -1,43 +1,77 @@
-import { NavLink } from "react-router-dom";
-import { useSelector, useDispatch } from "react-redux";
-import ProfileButton from './ProfileButton'
-import * as sessionActions from '../../store/session'
+import { NavLink } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import ProfileButton from './ProfileButton';
 
-function Navigation({isLoaded}){
-    const sessionUser = useSelector(state => state.session.user)
-    const dispatch = useDispatch()
-
-    const logout = (e) => {
-        e.preventDefault()
-        dispatch(sessionActions.logout())
-    }
-    const sessionLinks = sessionUser ? (
-        <>
-        <li>
-            <ProfileButton user={sessionUser}/>
-        </li>
-        <li>
-            <button onClick={logout}>Log Out</button>
-        </li>
-        </>
-    ): (
-        <>
-        <li>
-            <NavLink to="/login">Log In</NavLink>
-        </li>
-        <li>
-            <NavLink to='/signup'>Sign Up</NavLink>
-        </li>
-        </>
-    )
-    return (
-        <ul>
-            <li>
-                <NavLink to='/'>Home</NavLink>
-            </li>
-            {isLoaded && sessionLinks}
-        </ul>
-    )
+function Navigation({ isLoaded }){
+  const sessionUser = useSelector(state => state.session.user);
+//brings in navlinks and the profilebutton component
+  const sessionLinks = sessionUser ? (
+    <li>
+      <ProfileButton user={sessionUser} />
+    </li>
+  ) : (
+    <>
+      <li>
+        <NavLink to="/login">Log In</NavLink>
+      </li>
+      <li>
+        <NavLink to="/signup">Sign Up</NavLink>
+      </li>
+    </>
+  );
+//returns the navigation component and its components
+  return (
+    <ul>
+      <li>
+        <NavLink to="/">Home</NavLink>
+      </li>
+      {isLoaded && sessionLinks}
+    </ul>
+  );
 }
 
-export default Navigation
+export default Navigation;
+
+// import { NavLink } from "react-router-dom";
+// import { useSelector, useDispatch } from "react-redux";
+// import ProfileButton from './ProfileButton'
+// import * as sessionActions from '../../store/session'
+
+// function Navigation({isLoaded}){
+//     const sessionUser = useSelector(state => state.session.user)
+//     const dispatch = useDispatch()
+
+//     const logout = (e) => {
+//         e.preventDefault()
+//         dispatch(sessionActions.logout())
+//     }
+//     const sessionLinks = sessionUser ? (
+//         <>
+//         <li>
+//             <ProfileButton user={sessionUser}/>
+//         </li>
+//         <li>
+//             <button onClick={logout}>Log Out</button>
+//         </li>
+//         </>
+//     ): (
+//         <>
+//         <li>
+//             <NavLink to="/login">Log In</NavLink>
+//         </li>
+//         <li>
+//             <NavLink to='/signup'>Sign Up</NavLink>
+//         </li>
+//         </>
+//     )
+//     return (
+//         <ul>
+//             <li>
+//                 <NavLink to='/'>Home</NavLink>
+//             </li>
+//             {isLoaded && sessionLinks}
+//         </ul>
+//     )
+// }
+
+// export default Navigation
