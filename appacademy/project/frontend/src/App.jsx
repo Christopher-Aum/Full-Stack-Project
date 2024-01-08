ac309fe9563b2abd409d31f948dbc73016c9af3c
ChristopherAumonte <christopheraumonte@gmail.com>
2024-01-07 21:45:06 -0500

    Version 31

--- appacademy/project/frontend/src/App.jsx
+++ appacademy/project/frontend/src/App.jsx
@@ -1,9 +1,11 @@
 import { createBrowserRouter, RouterProvider, Outlet} from "react-router-dom";
 import LoginFormPage from "./components/LoginFormPage/LoginFormPage";
+import SignUpFormPage from "./components/SignupFormPage/SignUpFormPage";
 import {useState, useEffect} from 'react'
 import { useDispatch } from "react-redux";
 import * as sessionActions from './store/session'
 
+//function that checks if something is loaded, and dispatches the restoreUser function upon every rerender
 function Layout(){
   const dispatch = useDispatch();
   const [isLoaded, setIsLoaded] = useState(false);
@@ -20,7 +22,7 @@ function Layout(){
     </>
   )
 }
-
+//router handles the routing of locations based on the url
 const router = createBrowserRouter([
   {
     element: <Layout />,
@@ -30,7 +32,11 @@ const router = createBrowserRouter([
       element: <h1>Welcome!</h1>
     },
     {path:'/login',
-  element: <LoginFormPage />}
+  element: <LoginFormPage />},
+  {
+    path:'/signup',
+    element:<SignUpFormPage/>
+  }
     ]
   }
 ])
